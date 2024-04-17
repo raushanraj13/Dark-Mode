4fbae51f222abdf415fe1788df3b091ade580259
Raushan <rraushan723@gmail.com>
2024-04-18 00:40:38 +0530

    added: style.css

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ darkModeToggle.addEventListener('click', function () {
     const body = document.body;
     body.classList.toggle('dark-mode');
     content.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        modeImage.src = 'DarkMode.jpg';
+    } else {
+        modeImage.src = 'DayMode.jpg';
+    }
 });
